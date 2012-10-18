e06675a1a76f5a1d773073b741ee191a973875c0
neofish <lachlan@neofish.org>
2012-10-18 21:57:17 +1100

    Updated sh

--- sh/sh.c
+++ sh/sh.c
@@ -5,7 +5,26 @@
 
 #define BUF_LENGTH 4096
 
-void process_line(const char *line) {
+int process_line(const char *line) {
+  if (strcmp(line, "exit") == 0)
+    return 1;
+
+  if (strlen(line) < 1)
+    return 0;
+
+  // run the program
+  if (fork() == 0) {
+    // child
+    if (execv(strtok(line, " "), NULL) == -1) {
+      fprintf(stderr, "Error running program: %s\n", line);
+      return 0;
+    }
+
+  } else {
+    // parent
+    int status;
+    wait(&status);
+  }
 
 }
 
@@ -13,24 +32,49 @@ int interactive(void) {
   char line[BUF_LENGTH];
 
   do {
+
+    char *ps = getenv("PS1");
+    if (!ps)
+      ps = "# ";
+
+    printf("%s", ps); // print out the prompt
+
     if (fgets(line, BUF_LENGTH, stdin)) {
       line[strlen(line)-1] = 0;  // strip the \n
-      process_line(line);
+
+      // quit if needed
+      if (process_line(line) > 0)
+        return 0;
+
+    } else {
+      printf("\n");
+      return 1;
     }
 
-  } while (strcmp (line, "exit") != 0);
+  } while (1);
 
   return 0;
 }
 
 int main(int argc, char *argv[]) {
-
-#if DEBUG
-  printf("argc: %d\n", argc);
-#endif
   // if no shell file provided, go into default mode
   if (argc < 2)
     return interactive();
 
   // otherwise, run a file
+  FILE *file = fopen(argv[1], "r");
+  char line[BUF_LENGTH];
+
+  if (file) {
+    while (fgets(line, BUF_LENGTH, file)) {
+      line[strlen(line)-1] = 0;  // strip the \n
+
+      // quit if needed
+      if (process_line(line) > 0)
+        return 0;
+
+    }
+
+    fclose(file);
+  }
 }
