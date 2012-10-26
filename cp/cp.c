6f60f3c694b70b2f5205748ff14840e39584a8f1
neofish <lachlan@neofish.org>
2012-10-26 14:56:10 +1100

    Added more code

--- cp/cp.c
+++ cp/cp.c
@@ -1,7 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 
 int main(int argc, char *argv[]) {
 
+  if (argc < 3) {
+    fprintf(stderr, "usage: %s src dest\n", argv[0]);
+    exit(1);
+  }
+
+  if (access(argv[1], R_OK) != -1) {
+    fprintf(stderr, "error: could not access %s\n", argv[1]);
+    exit(1);
+  }
+
+
+  FILE *src = open(argv[1], O_RDONLY);
+
+  FILE *dest;
+  char buf[256];
+  int sz = 0;
+
+  if (dest = open(argv[2], O_CREAT | O_WRONLY) != NULL) {
+
+    do {
+      if (sz = read(src, (void *)buf, 256) > 0) {
+
+        if (write(dest, (void *)buf, sz) > 0) {
+          fprintf(stderr, "error writing to %s\n", argv[2]);
+          exit(1);
+        }
+
+      } else {
+        fprintf(stderr, "error reading from %s\n", argv[1]);
+        exit(1);
+    } while (sz > 0);
+  }
   return 0;
 }
